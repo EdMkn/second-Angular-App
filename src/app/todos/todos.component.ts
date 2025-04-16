818265b11e4a1057bce5b748016615835509aefe
Manyim Ma Makon <nmaknyim@gmail.com>
2025-04-16 20:01:25 +0200

    creation of pipes

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { catchError } from 'rxjs';
 import { Todo } from '../model/todo.type';
+import {NgIf} from '@angular/common';
+import { TodoItemComponent } from "../components/todo-item/todo-item.component";
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [NgIf, TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
@@ -14,15 +16,30 @@ export class TodosComponent implements OnInit{
   todoItems = signal<Array<Todo>>([]);
 
   ngOnInit(): void {
-    this.todoService.getTodosFromApi()
-    .pipe(
-      catchError((err) => {
-        console.log(err);
-        throw err;
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos)
+      });
+    }
+
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if(todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed:!todo.completed,
+          };
+        }
+        return todo;
       })
-    )
-    .subscribe((todos) => {
-      this.todoItems.set(todos)
     })
   }
 }
