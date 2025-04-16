2f518910ed34c0ed1e73ce99d70f50e89d9d22fc
Manyim Ma Makon <nmaknyim@gmail.com>
2025-04-16 20:19:36 +0200

    completion of a pipe for filter

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,18 +2,21 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { catchError } from 'rxjs';
 import { Todo } from '../model/todo.type';
-import {NgIf} from '@angular/common';
 import { TodoItemComponent } from "../components/todo-item/todo-item.component";
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
-  imports: [NgIf, TodoItemComponent],
+  standalone: true,
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit{
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todoService
